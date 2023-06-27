47411f1aa717427ff7ec2d91208c57a83b7e7456
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-06-27 01:53:36 +0000

    Create Try.py

---
+++ Try.py
@@ -0,0 +1,19 @@
+class Try:
+
+    def obtainInput(self):
+        userInput1 = input('Please input a number:')
+        userInput2 = input('Please enter another number:')
+        return userInput1, userInput2
+
+    def addNumbers(self):
+        userInput1, userInput2 = self.obtainInput()
+
+        try:
+            return print(int(userInput1)+ int(userInput2))
+
+        except:
+            return print('Error: user input contains a symbol other than a number.')
+
+    
+t = Try()
+t.addNumbers()
